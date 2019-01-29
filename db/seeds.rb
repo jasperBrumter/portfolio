550ff420c97c9a7defdda7127a4d05f57a2cb313
Jasper Brumter <jasper.brumter@gmail.com>
2019-01-29 16:58:14 +0100

    improve nightsky

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,35 +12,35 @@ Project.destroy_all
 puts "adding new projects"
 Project.create({
 	name: "Travel Album",
-	description: "Responsive website for showcasing travel pictures. Includes a slideshow coded entirely in Javascript (no Bootstrap at all). Disclaimer: these are not my pictures!",
+	description: "Responsive website for showcasing travel pictures. Includes a slideshow coded entirely in Javascript (no Bootstrap at all). By the way, I didn't take these pictures, but I've visited all of these countries!",
 	image: "heroku.png",
 	link: "https://travel-album.herokuapp.com"
 });
 
 Project.create({
 	name: "Scrabble Helper",
-	description: "Tired of losing at Scrabble? Train or Cheat your way to Scrabble fame with this web app. Exists in French and English!",
+	description: "Tired of losing at Scrabble (like me)? Train or Cheat your way to Scrabble fame with this web app. Use this to find the longest word with your current letters. Developed by myself in French and English!",
 	image: "scrabble.png",
 	link: "https://scrabbled-eggs.herokuapp.com"
 })
 
 Project.create({
 	name: "Crewin",
-	description: "Corporate website for upcoming company.",
+	description: "Corporate website for upcoming company. Why use Wordpress when you can get me to make your website for virtually nothing?",
 	image: "heroku.png",
 	link: "#"
 })
 
 Project.create({
 	name: "Liar's Dice",
-	description: "Coded entirely in Ruby, play Liar's Dice (aka. Perudo) on Command Line against your computer! Choose the number of opponents and off you go! Read github wiki for more info.",
+	description: "Coded entirely in Ruby, play Liar's Dice (aka. Perudo) on Command Line against your computer! Choose the number of opponents and off you go! The CPU's decisions are saved in a CSV file in order to improve its decision-making. Read github wiki for more info.",
 	image: "ruby.png",
 	link: "https://github.com/jasperBrumter/liars-dice.ruby"
 })
 
 Project.create({
 	name: "Rails liar",
-	description: "A version of Liar's Dice on website. Work in progress, working on React.js in order to play entire game without refreshing.",
+	description: "A version of Liar's Dice on website. Work in progress, currently working on React.js in order to play entire game without refreshing. ps: did you notice the palindrome?",
 	image: "rails.png",
 	link: "#"
 })
