d85786656c1a372f870be1a6046cdd502eb167f2
Jasper Brumter <jasper.brumter@gmail.com>
2019-03-21 02:02:06 -0400

    ecard start

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,7 @@ class ProjectsController < ApplicationController
   	works = Working.all.count
   	@works = Working.all.sample(works)
   end
+
+  def ecard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to:'projects#index'
+  get "/ecard", to: "projects#ecard", as: :ecard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
